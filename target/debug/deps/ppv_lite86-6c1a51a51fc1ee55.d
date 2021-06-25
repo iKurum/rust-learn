314a8415e4a3402b0e9a578b296591da77046370
ikurum <cyan@ikurum.cn>
2021-06-25 13:55:39 +0800

    Delete target directory

--- target/debug/deps/ppv_lite86-6c1a51a51fc1ee55.d
+++
@@ -1,9 +0,0 @@
-D:\code\rust\learn\target\debug\deps\ppv_lite86-6c1a51a51fc1ee55.rmeta: C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\lib.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\soft.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\types.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\mod.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\sse2.rs
-
-D:\code\rust\learn\target\debug\deps\ppv_lite86-6c1a51a51fc1ee55.d: C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\lib.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\soft.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\types.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\mod.rs C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\sse2.rs
-
-C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\lib.rs:
-C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\soft.rs:
-C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\types.rs:
-C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\mod.rs:
-C:\Users\Admin\.cargo\registry\src\mirrors.ustc.edu.cn-61ef6e0cd06fb9b8\ppv-lite86-0.2.10\src\x86_64\sse2.rs:
--- target/rls/debug/deps/learn-bb05c782fd4964d4.d
+++
@@ -1,5 +0,0 @@
-d:\code\rust\learn\target\rls\debug\deps\learn-bb05c782fd4964d4.rmeta: src\main.rs
-
-d:\code\rust\learn\target\rls\debug\deps\learn-bb05c782fd4964d4.d: src\main.rs
-
-src\main.rs:
